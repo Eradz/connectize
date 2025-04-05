b2b52f95f2c794ce098c75bf3fbe07542aab2dfe
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-04-05 22:52:40 +0100

    modified vite.config.mjs in .

--- vite.config.mjs
+++ vite.config.mjs
@@ -12,19 +12,5 @@ export default defineConfig(({ mode }) => ({
     }),
   ],
   server: { port: 3000 },
-  build: {
-    outDir: "build",
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("lodash")) return "lodash";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
+  build: { outDir: "build" },
 }));
