809e7b850db56eebd5263d8bc9b960e89724aa0b
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-04-05 23:55:25 +0100

    modified 9 files

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import compression from "vite-plugin-compression";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    compression({
-      algorithm: "gzip",
-      threshold: 10240,
-      deleteOriginalAssets: mode === "capacitor",
-    }),
-  ],
+  plugins: [react()],
   server: { port: 3000 },
   build: { outDir: "build" },
 }));
