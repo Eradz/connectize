146e976ac3b9ba58f52df38ee19f8ceb385a2b3f
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-04-05 21:50:34 +0100

    modified 3 files

--- src/pages/authentication/AuthLayout.jsx
+++ src/pages/authentication/AuthLayout.jsx
@@ -3,22 +3,26 @@ import { motion } from "framer-motion";
 import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import Logo from "../../components/logo";
+import LightParagraph from "../../components/ParagraphText";
 import { getSession } from "../../lib/session";
 
 function AuthLayout({ redirectUrl = "/" }) {
   const session = getSession();
   const { pathname } = useLocation();
 
+  const isLogin = pathname === "/login";
+  const isRegister = pathname === "/signup";
+
+  const isLoginOrRegister = isLogin || isRegister;
+
   if (session) {
-    return (
-      <Navigate to={pathname === "/login" ? "/profile" : redirectUrl} replace />
-    );
+    return <Navigate to={isLogin ? "/profile" : redirectUrl} replace />;
   }
 
   return (
     <main className="flex justify-center h-screen p-4">
       <section className="size-full max-h-screen overflow-y-auto flex flex-col items-center lg:!w-1/2 shrink-0 scrollbar-hidden">
-        <section className="w-full max-xs:h-full py-4 max-w-sm lg:max-w-md">
+        <section className="w-full max-xs:h-full  py-4 max-w-sm lg:max-w-md">
           <motion.section
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -35,12 +39,12 @@ function AuthLayout({ redirectUrl = "/" }) {
               className="max-w-screen-md text-[1.9rem] leading-tight font-bold"
               key={pathname + "heading"}
             >
-              {pathname === "/login" ? (
+              {isLogin ? (
                 <>
                   Connect, Collaborate and Thrive{" "}
                   <span className="text-gold ">with Connectize</span>
                 </>
-              ) : pathname === "/signup" ? (
+              ) : isRegister ? (
                 <>
                   <span className="text-gold font-bold">Connectize</span>{" "}
                   bridges the gap between interactions and transactions within
@@ -55,10 +59,21 @@ function AuthLayout({ redirectUrl = "/" }) {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.35 }}
-            className="size-full mt-4"
+            className="mt-4"
             key={pathname + "outlet"}
           >
             <Outlet />
+          </motion.section>
+
+          <motion.section
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ delay: 0.45 }}
+            className="container flex items-center justify-center gap-2 font-bold mt-8"
+            key={pathname + "endorsement"}
+          >
+            <img src="/images/nuprc-logo.png" className="h-8" />
+            <LightParagraph>Endorsed by NUPRC</LightParagraph>
           </motion.section>
         </section>
       </section>
