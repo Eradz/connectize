d6e050978348b32ae7eeb6ed7d51544ab8c22d39
clever akanimoh <cleverakanimoh02@gmail.com>
2025-05-20 02:47:23 +0100

    modified MessageControl.jsx and messaging.jsx in messages

--- src/components/messages/MessageControl.jsx
+++ src/components/messages/MessageControl.jsx
@@ -201,7 +201,7 @@ export default function MessageControl({ loading, recipientId, senderId }) {
   }, [message]);
 
   return (
-    <section className="bg-white p-1 px-4 rounded-md flex flex-col gap-2 transition-all duration-300 sticky bottom-14 md:bottom-0">
+    <section className="bg-white p-1 px-4 rounded-md flex flex-col gap-2 transition-all duration-300 sticky bottom-14 md:bottom-4">
       {/* valid images */}
       {validImages && (
         <ValidImages
