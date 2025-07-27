643013ae8be7a89e8c6c78052e3b4f164f8dae7b
dynasty <kydiamond33@gmail.com>
2025-07-27 19:32:09 +0100

    Completed merge with fix/product_upadte

--- src/components/admin/markets/newlyListed.jsx
+++ src/components/admin/markets/newlyListed.jsx
@@ -5,8 +5,6 @@ import { motion } from "framer-motion";
 import { Link, useSearchParams } from "react-router-dom";
 import { getProducts } from "../../../api-services/products";
 import { useAuth } from "../../../context/userContext";
-import { usePollAllCompanies } from "../../../hooks/usePolling";
-import { useProductImages } from "../../../hooks/useProduct";
 import { webRoutes } from "../../../lib/webRoutes";
 import CustomTabs from "../../custom/tabs";
 import PrimaryButton from "../../PrimaryButton";
@@ -15,39 +13,12 @@ function NewlyListed({ companyId }) {
   return (
     <section className="container">
       <CustomTabs
-<<<<<<< HEAD
         tabsHeading={["All Products", "Newly Listed"]}
         tabsPanels={[
           // used `|| undefined` because if the `companyId` is an empty string it would still be falsy, and it would be sent to the server as an empty string i.e `?company=""`
           <DisplayAllProducts companyId={companyId || undefined} />,
           <DisplayNewlyListedProducts companyId={companyId || undefined} />,
         ]}
-=======
-        tabsHeading={tabs.map((tab) => tab.label)}
-        tabsPanels={tabs.map((tab, index) => (
-          <div
-            key={index}
-            className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4"
-          >
-            {isLoading
-              ? Array.from({ length: 6 }, (_, index) => (
-                  <ListCardSkeleton key={index} />
-                ))
-              : tab.products.map((product) => {
-                  return (
-                    <ProductListCard
-                      key={product.id}
-                      id={product.id}
-                      image={product?.images?.[0]?.image}
-                      title={product.title}
-                      subtitle={product.category}
-                      companyName={product?.company?.company_name || ""}
-                    />
-                  );
-                })}
-          </div>
-        ))}
->>>>>>> fix/product_update
       />
     </section>
   );
@@ -213,6 +184,8 @@ export const ProductListCard = ({
 
   // const { productImage } = useProductImages(id);
   // const imageUrl = productImage?.[0]?.image || image || "";
+  // const { productImage } = useProductImages(id);
+  // const imageUrl = productImage?.[0]?.image || image || "";
 
   return (
     <motion.div
@@ -226,7 +199,6 @@ export const ProductListCard = ({
         }
       )}
     >
-<<<<<<< HEAD
       <Link to={"/products/" + id} className="w-full">
         <img
           src={image}
@@ -236,15 +208,6 @@ export const ProductListCard = ({
           alt={title || "Product"}
         />
       </Link>
-=======
-      <img
-        src={image}
-        className={clsx("w-full h-[300px] rounded-lg", {
-          "md:h-[200px]": isSummary,
-        })}
-        alt={title || "Product"}
-      />
->>>>>>> fix/product_update
       <div className="flex sm:flex-col items-start justify-between gap-4 sm:!gap-2 w-full">
         <div>
           <Link
