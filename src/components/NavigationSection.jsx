78694b392d35ee2d20367465fd0d54f5c00c0cac
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2025-12-23 20:47:50 +0100

    fix profile routing

--- src/components/NavigationSection.jsx
+++ src/components/NavigationSection.jsx
@@ -205,7 +205,7 @@ export function NavigationSection({ hasHeader, isSmallNavigation = false }) {
           return (
             <li key={index}>
               <Link
-                to={to}
+                to={name === "Profile" ? `/co/${currentUser?.id}` : to}
                 onClick={() => toggleNav(false)}
                 className={clsx(
                   "flex gap-2 items-center transition-all active:scale-90 duration-300 p-2 py-2.5 xs:hover:!text-mid_grey !text-sm flex-col text-xs xs:text-[.65rem]",
@@ -259,7 +259,7 @@ export function NavigationSection({ hasHeader, isSmallNavigation = false }) {
             return (
               <li key={index}>
                 <Link
-                  to={to}
+                  to={name === "Profile" ? `/co/${currentUser?.id}` : to}
                   onClick={() => toggleNav(false)}
                   className={clsx(
                     "flex gap-2 items-center transition-all active:scale-90 duration-300 p-2 py-2.5 xs:hover:!text-mid_grey !text-sm rounded",
