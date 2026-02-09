c01215fb487edfe1dabfe96bbbe2542ec3bd2771
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2026-02-09 14:59:30 +0100

    fix: professional icons

--- src/components/workforce/JobCard.jsx
+++ src/components/workforce/JobCard.jsx
@@ -94,7 +94,7 @@ return(
           <div className="flex-1">
             <div className="flex justify-between items-center space-x-3 mb-2">
                 <div className="flex gap-2">
-                    <div className="bg-[#FFF1C6] p-2 rounded-lg">
+                    <div className="bg-[#FFF1C6] p-2 rounded-lg h-fit w-fit">
                         {getJobTypeIcon(job.job_type)}
                     </div>
                         <h3 className="font-semibold text-gray-900 text-lg">{job.title}</h3>
