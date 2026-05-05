84c30c08bd955bd1bc0731719c5fa46458f1d264
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2026-05-05 11:38:46 +0100

    fix: like button on web

--- src/components/admin/feeds/DiscoverFeed.jsx
+++ src/components/admin/feeds/DiscoverFeed.jsx
@@ -16,7 +16,6 @@ const DiscoverFeed = () => {
     setUser(currentUser);
   }, [currentUser, setUser]);
 
-  console.log("Current User:", currentUser);
   return (
     <section className="">
       <section className="flex items-baseline gap-2 max-sm:px-4 sm:container mt-2">
